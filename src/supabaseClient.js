d316b3b3744ef160c749e7455b16b45b2f110e4b
Insova <jack@insova.ie>
2026-08-18 20:36:26 +0100

    Okay lets try this

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -9,8 +9,8 @@ import { createClient } from '@supabase/supabase-js';
   production. Create React App only exposes variables prefixed
   REACT_APP_.
 */
-const url = process.env.REACT_APP_SUPABASE_URL;https://xupgmjomufmxhiwljbev.supabase.co/rest/v1/
-const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cGdtam9tdWZteGhpd2xqYmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNzA3MTMsImV4cCI6MjEwMjY0NjcxM30.GSGie4yXng6MTFBLFBVBgjcN9lmAUvPLyQkrVz4DCok
+const url = process.env.REACT_APP_SUPABASE_URL;'https://xupgmjomufmxhiwljbev.supabase.co/rest/v1/'
+const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cGdtam9tdWZteGhpd2xqYmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNzA3MTMsImV4cCI6MjEwMjY0NjcxM30.GSGie4yXng6MTFBLFBVBgjcN9lmAUvPLyQkrVz4DCok'
 
 export const configured = Boolean(url && anonKey);
 
